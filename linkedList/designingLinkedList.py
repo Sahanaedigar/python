eec65bc538b19764d9ce907c7a4cfb763fba7caa
Sahana Edigar HP <86426895+Sahanaedigar@users.noreply.github.com>
2025-08-05 23:33:03 +0530

    Add files via upload

--- linkedList/designingLinkedList.py
+++ linkedList/designingLinkedList.py
@@ -10,6 +10,26 @@ class LinkedList:
     def __init__(self):
         self.head = None  # head of the list (first node)
 
+    def addAtHead(self, val: int) -> None:
+        mynode = Node(val)         # create a new node
+        mynode.next = self.head    # point new node to current head
+        self.head = mynode         # move head to new node
+        self.size += 1             # increase size
+
+    def addAtTail(self, val: int) -> None:
+        mynode = Node(val)         # create new node
+
+        if self.head is None:      # if list is empty
+            self.head = mynode     # new node becomes head
+        else:
+            curr = self.head
+            while curr.next is not None:  # go to last node
+                curr = curr.next
+            curr.next = mynode     # connect last node to new node
+        self.size += 1
+
+
+
 
 
 '''
